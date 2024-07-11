9d10b2ef110d74a56935f08167f0861ae35d6576
AbhijithKK <abhijithkkpurameri@gmail.com>
2024-07-11 16:50:25 +0530

    chatwindow content layout done

--- Client/src/Components/ChatWindowComponents/ChatSpace/ChatSpace.tsx
+++ Client/src/Components/ChatWindowComponents/ChatSpace/ChatSpace.tsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import './ChatSpace.css'
 
 const ChatSpace = () => {
   return (
-    <div>
-      ss
+    <div className='chat-space-container'>
+      <div className="receive-message">
+       <p> rece   ivemsgddddddd  dddddddddddddddddddddddddddddddddddddddddddddddd</p>
+      </div>
+      <div className="send-message">
+       <p> send message</p>
+      </div>
     </div>
   )
 }
