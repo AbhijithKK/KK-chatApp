9288eb4622f2866644162cd78554be76597ce9e5
AbhijithKK <abhijithkkpurameri@gmail.com>
2024-07-12 15:17:10 +0530

    send and fie picker icon added

--- Client/src/Components/ChatWindowComponents/InputSection/InputSection.tsx
+++ Client/src/Components/ChatWindowComponents/InputSection/InputSection.tsx
@@ -1,15 +1,17 @@
 import './InputSection.css'
+import sendbtn from '../../../assets/icons8-send-button-100.png'
+import filepicer from '../../../assets/icons8-add-file-50.png'
 const InputSection = () => {
   return (
     <div className='input-section-container'>
          <div className="media-picker"> 
-         <img src="" alt='mediapicker' />
+         <img src={filepicer} alt='mediapicker' />
          </div>
          <div className="input-box">
           <textarea name="" id="" ></textarea>
          </div>
          <div className="send-btn">
-          <button>send</button>
+          <button><img src={sendbtn} alt="send" /></button>
          </div>
     </div>
   )
