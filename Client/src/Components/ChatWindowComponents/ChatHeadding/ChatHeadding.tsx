7d4c1a93bb533a7c7fa7a4ebf49f52f135ee11d6
AbhijithKK <abhijithkkpurameri@gmail.com>
2024-07-12 15:05:05 +0530

    chat page  icons added

--- Client/src/Components/ChatWindowComponents/ChatHeadding/ChatHeadding.tsx
+++ Client/src/Components/ChatWindowComponents/ChatHeadding/ChatHeadding.tsx
@@ -1,18 +1,20 @@
 import './ChatHeadding.css'
-
+import backbtn from '../../../assets/icons8-back-button-50.png'
+import dummypro from '../../../assets/icons8-test-account-48.png'
+import optionsicon from '../../../assets/icons8-menu-button-30.png'
 const ChatHeadding = () => {
   return (
     <div className='chatheadding-container'>
       <div className="profile-pic">
-        <img src="" alt="backbtn" />
-        <img src="" alt="propic" />
+        <img src={backbtn} alt="backbtn" />
+        <img src={dummypro} alt="propic" />
       </div>
       <div className="profile-name">
         <p>name</p>
         <p>status</p>
       </div>
       <div className="profile-options">
-        <img src="" alt="options" />
+        <img src={optionsicon} alt="options" />
       </div>
     </div>
   )
