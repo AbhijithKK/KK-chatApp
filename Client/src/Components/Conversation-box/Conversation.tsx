497fa6e718a0ff9a26b16fc2e065e50ee8d04687
AbhijithKK <abhijithkkpurameri@gmail.com>
2024-07-12 15:34:59 +0530

    allusers component css border added

--- Client/src/Components/Conversation-box/Conversation.tsx
+++ Client/src/Components/Conversation-box/Conversation.tsx
@@ -1,10 +1,11 @@
 import './Conversation.css'
+import proicon from '../../assets/icons8-test-account-48.png'
 const Conversation = () => {
   return (
     <div className='conversation-container'>
       
     <div className="conversation-left">
-        <img src="" alt="logo" />
+        <img src={proicon} alt="logo" />
     </div>
     <div className="conversation-right">
     <div className="conversation-name">
