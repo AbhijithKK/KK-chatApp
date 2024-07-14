120db97a53cf9b8122a2067156ed8a18b4df7d1b
AbhijithKK <abhijithkkpurameri@gmail.com>
2024-07-14 22:48:59 +0530

    react routes setup successfully

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,21 +1,19 @@
-
-import './App.css'
-import LandingPage from './Components/LandingPage/LandingPage'
-import Login from './Components/LoginPage/Login'
-import Signup from './Components/SignupPage/Signup'
+import "./App.css";
+import LandingPage from "./Components/LandingPage/LandingPage";
+import Login from "./Components/LoginPage/Login";
+import Signup from "./Components/SignupPage/Signup";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
-
   return (
     <>
-      <div>
-      {/* <Signup/> */}
-      <Login/>
-        {/* <LandingPage/> */}
-      </div>
-        
+      <Routes>
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/" element={<Login />} />
+        <Route path="/home" element={<LandingPage />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
