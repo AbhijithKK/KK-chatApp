c7e11864101d817414b37e2c315246feaa8bd8d6
AbhijithKK <abhijithkkpurameri@gmail.com>
2024-07-17 12:41:18 +0530

    provaider setp to main page

--- Client/src/main.tsx
+++ Client/src/main.tsx
@@ -1,16 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import {
-  BrowserRouter as Router,
- 
-} from "react-router-dom";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "./Components/Utils/Redux/Store.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Router>
-    <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </Router>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
