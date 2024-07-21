f170bf7a97dd4248d469d6f5b6031f92cee2a9dd
AbhijithKK <abhijithkkpurameri@gmail.com>
2024-07-22 01:03:08 +0530

    logout route and controller created

--- Server/Routers/userRouter.js
+++ Server/Routers/userRouter.js
@@ -1,4 +1,4 @@
-import { allUserData, allUsers,  checkAuth,  login, signup, userDetails, userUpdate, verifyOtp } from "../Controllers /userControllers.js";
+import { allUserData, allUsers,  checkAuth,  logOut,  login, signup, userDetails, userUpdate, verifyOtp } from "../Controllers /userControllers.js";
 import  Express  from 'express'
 import  {auth} from "../Middleware/Auth.js";
 import { upload } from "../Helpers/multer.js";
@@ -11,6 +11,7 @@ router.post('/verifyotp',verifyOtp)
 router.post('/userdata',auth,allUserData)
 router.put('/userupdate',auth,upload.single('image'),userUpdate)
 router.get('/alluser',auth,allUsers)
+router.get('/logout',auth,logOut)
 router.get('/checkauth',checkAuth)
 router.get('/userdetails',auth,userDetails)
 export default router  
